231b27e27038a0148e112d57c8b3f80720bda9bd
[ARNAV] <arnavgoyal1317@gmail.com>
2025-08-22 19:00:34 +0530

    Added Hero + some changed one in navbar

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,8 +1,20 @@
 
 const Hero = () => {
   return (
-    <div className='h-screen w-full '>
-      
+    <div className="h-screen w-full bg-[url('https://images.pexels.com/photos/1438081/pexels-photo-1438081.jpeg')] 
+    bg-no-repeat bg-center bg-cover flex flex-col justify-center items-center gap-9">
+        <div className="h-40 w-40 mt-24">
+
+      <img src="https://rha.socet.edu.in/img/redhat.png" alt="" />
+        </div>
+        <h1 className="text-3xl font-bold text-white">Bridge the gap between education and industry</h1>
+        <div className="text-xl font-bold text-white flex flex-col justify-center items-center">
+            <h1>Red Hat Academy turns academic institutions into centers for enterprise-ready talent</h1>
+            <h1 >by outfitting them with Red Hat training and certification.</h1>
+        </div>
+
+        <button className="bg-transparent border-2 font-semibold hover:bg-red-500 hover:transition-colors hover:duration-700 text-white border-red-700 px-8 py-2 rounded-3xl"><a href="">About RHA - SOGI</a></button>
+
     </div>
   )
 }
