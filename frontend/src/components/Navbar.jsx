998d8a85cdc3e622b2cf384f8b8f28d27f4da1ee
Hitesh <hiteshkumarnain@gmail.com>
2025-08-23 14:27:31 +0530

    modified navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
-import {navbarOptions} from "../utils/NavbarUtils";
-import { Link } from "react-router-dom";
+import { navbarOptions } from "../utils/NavbarUtils";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 function Navbar() {
   const [scrolled, setScrolled] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -24,31 +26,54 @@ function Navbar() {
       element.scrollIntoView({ behavior: "smooth" });
     }
   };
-  
+
+  const handleNavClick = (option) => {
+    if (option.id === "hero") {
+      if (location.pathname === "/") {
+        scrollToSection(option.id);
+      } else {
+        navigate("/");
+      }
+    } else {
+      if (location.pathname === "/") {
+        scrollToSection(option.id);
+      } else {
+        navigate("/", { state: { scrollToId: option.id } });
+      }
+    }
+  };
+
   const styleProp = `relative after:content-[''] after:absolute after:left-0 after:bottom-[-4px] 
-               after:h-[3px] after:w-0 after:bg-red-800 
-               after:transition-all after:duration-300 
-               hover:after:w-full hover:text-white text-gray-300 font-semibold`
+                    after:h-[3px] after:w-0 after:bg-red-800 
+                    after:transition-all after:duration-300 
+                    hover:after:w-full hover:text-white text-gray-300 font-semibold`;
 
   return (
     <div
-      className={`fixed  top-0 left-0 z-40 w-full p-4  duration-500 transition-all flex justify-around items-center ${
+      className={`fixed top-0 left-0 z-40 w-full p-4 duration-500 transition-all flex justify-around items-center ${
         scrolled ? "bg-gray-900 text-white shadow-lg h-20 " : "bg-transparent text-black h-28"
       }`}
     >
-        <div>
-        <img src="https://rha.socet.edu.in/img/logo.png" className="h-12 w-36" alt=""  />
-
-        </div>
-        <div className="flex gap-6 text-black">
-            {
-                navbarOptions.map((option)=>(
-                    <button onClick={() => scrollToSection(option.id)} href={`#${option}`} key={option} className={`${styleProp}`}>{option.label}</button>
-                ))
-            }
-            <Link to={"/signup"}  className={`${styleProp}`}>SignUp</Link>
-            <Link to={"/login"} className={`${styleProp}`}>login</Link>
-        </div>
+      <div>
+        <img src="https://rha.socet.edu.in/img/logo.png" className="h-12 w-36" alt="" />
+      </div>
+      <div className="flex gap-6 text-black">
+        {navbarOptions.map((option) => (
+          <button
+            onClick={() => handleNavClick(option)}
+            key={option.id}
+            className={`${styleProp}`}
+          >
+            {option.label}
+          </button>
+        ))}
+        <Link to="/signup" className={`${styleProp}`}>
+          SignUp
+        </Link>
+        <Link to="/login" className={`${styleProp}`}>
+          Login
+        </Link>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,22 +1,38 @@
-import About from "../components/About"
-import Contact from "../components/Contact"
-import Events from "../components/Events"
-import FAQ from "../components/FAQ"
-import Footer from "../components/Footer"
-import Gallery from "../components/Gallery"
-import Hero from "../components/Hero"
-import Info from "../components/Info"
-import Mentors from "../components/Mentors"
-import Navbar from "../components/Navbar"
-import Technical from "../components/Technical"
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import About from "../components/About";
+import Contact from "../components/Contact";
+import Events from "../components/Events";
+import FAQ from "../components/FAQ";
+import Footer from "../components/Footer";
+import Gallery from "../components/Gallery";
+import Hero from "../components/Hero";
+import Info from "../components/Info";
+import Mentors from "../components/Mentors";
+import Navbar from "../components/Navbar";
+import Technical from "../components/Technical";
 
 const MainPage = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.state?.scrollToId) {
+      const element = document.getElementById(location.state.scrollToId);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+      // Clear state so scrolling doesn't repeat on back/forward navigation
+      window.history.replaceState({}, document.title);
+    }
+  }, [location]);
+
   return (
     <div className="min-h-screen w-full overflow-x-hidden relative">
-      <div className="fixed top-0 left-0 w-full h-[130vh] -z-10 
-        bg-[url('https://images.pexels.com/photos/33507079/pexels-photo-33507079.jpeg')] 
-        bg-cover bg-center">
-      </div>
+      <div
+        className="fixed top-0 left-0 w-full h-[130vh] -z-10 
+          bg-[url('https://images.pexels.com/photos/33507079/pexels-photo-33507079.jpeg')] 
+          bg-cover bg-center"
+      ></div>
 
       <Navbar />
 
@@ -39,7 +55,7 @@ const MainPage = () => {
         <Footer />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainPage
+export default MainPage;
